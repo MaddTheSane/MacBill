1b8cdb85c74c0998c2a5326fd109e6d88371b0ab
C.W. Betts <computers57@hotmail.com>
2015-05-25 16:51:28 -0600

    Begin work on porting StepBill to Swift.

---
+++ SwiftBill/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftBill
+//
+//  Created by C.W. Betts on 5/25/15.
+//  Copyright (c) 2015 C.W. Betts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
